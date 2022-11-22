6f2ff9309b7e525fbe7d2e4ee2cedfdbda1678ab
Jasmin Wagner <jwagner@student.tgm.ac.at>
2022-11-22 08:56:51 +0100

    atmen übungen

--- ghostcovid/src/views/Atmen.vue
+++ ghostcovid/src/views/Atmen.vue
@@ -9,39 +9,52 @@
         <a href="#training/spiele" class="startbutton">Let's <b style="font-family:Chalkduster; letter-spacing:1px;">breath</b> together!</a>
       </section>
       <section id="training/spiele" class="contenthome">
-        <p class="lets">Übung 1</p>
+        <p class="lets">Brustatmung</p>
         <p class="hometext">
-          Wir bieten eine Auswahl an verschiedenen Spielen, welche dein Gedächtnis und deine Merkfähigkeit wieder in Schwung bringen.<br>
-          In unserem Podcast erzählen Betroffene, wie es ihnen mit der Situation geht und was ihnen besonders geholfen hat. <br>
-          Außerdem könnt ihr euch selbst in unserem Forum mit Gleichgesinnten austauschen und Erfahrungen sammeln bzw. weitergeben. <br>
+          Setz dich aufrecht hin und lege eine Handfläche auf deine obere Brusthälfte.<br>
+          Schließe deine Augen und versuche, gezielt in die Brust zu atmen also nur in den oberen Bereich deiner Lunge.<br>
+          Atme durch die Nase ein und durch den Mund langsam aus, so als würdest du viele Kerzen ausblasen müssen.<br>
+          Wiederhole dies nun fünf Mal, ehe du eine kurze Pause machst. Mache einen zweiten Durchgang.<br>
         </p>
         <p class="lets">-</p>
-        <p class="lets">Übung 1</p>
+        <p class="lets">Bauchatmung</p>
         <p class="hometext">
-          Wir bieten eine Auswahl an verschiedenen Spielen, welche dein Gedächtnis und deine Merkfähigkeit wieder in Schwung bringen.<br>
-          In unserem Podcast erzählen Betroffene, wie es ihnen mit der Situation geht und was ihnen besonders geholfen hat. <br>
-          Außerdem könnt ihr euch selbst in unserem Forum mit Gleichgesinnten austauschen und Erfahrungen sammeln bzw. weitergeben. <br>
+          Setz dich aufrecht hin und lege eine Handfläche auf deinen Bauch.<br>
+          Schließe deine Augen und versuche, gezielt in den Bauch zu atmen, so dass sich dieser deutlich hebt und senkt.<br>
+          Atme durch die Nase ein und durch den Mund langsam aus, so als würdest du viele Kerzen ausblasen müssen.<br>
+          Wiederhole dies nun fünf Mal, ehe du eine kurze Pause machst. Mache einen zweiten Durchgang.<br>
         </p>
         <p class="lets">-</p>
-        <p class="lets">Übung 1</p>
+        <p class="lets">Rückenatmung</p>
         <p class="hometext">
-          Wir bieten eine Auswahl an verschiedenen Spielen, welche dein Gedächtnis und deine Merkfähigkeit wieder in Schwung bringen.<br>
-          In unserem Podcast erzählen Betroffene, wie es ihnen mit der Situation geht und was ihnen besonders geholfen hat. <br>
-          Außerdem könnt ihr euch selbst in unserem Forum mit Gleichgesinnten austauschen und Erfahrungen sammeln bzw. weitergeben. <br>
+          Setz dich aufrecht hin und stütze dich auf deinen Knien ab. Mache einen Katzenbuckel.<br>
+          Schließe deine Augen und versuche, gezielt in den Rücken zu atmen und einen Buckel zu machen.<br>
+          Atme durch die Nase ein und durch den Mund langsam aus, so als würdest du viele Kerzen ausblasen müssen.<br>
+          Wiederhole dies nun fünf Mal, ehe du eine kurze Pause machst. Mache einen zweiten Durchgang.<br>
         </p>
         <p class="lets">-</p>
-        <p class="lets">Übung 1</p>
+        <p class="lets">Flankenatmung</p>
         <p class="hometext">
-          Wir bieten eine Auswahl an verschiedenen Spielen, welche dein Gedächtnis und deine Merkfähigkeit wieder in Schwung bringen.<br>
-          In unserem Podcast erzählen Betroffene, wie es ihnen mit der Situation geht und was ihnen besonders geholfen hat. <br>
-          Außerdem könnt ihr euch selbst in unserem Forum mit Gleichgesinnten austauschen und Erfahrungen sammeln bzw. weitergeben. <br>
+          Setz dich aufrecht hin und lege deine Handflächen auf deine Flanken (seitlich am Rumpf im Brustbereich).<br>
+          Schließe deine Augen und versuche, gezielt in die Seiten zu atmen, so dass sich diese nach außen ausdehnen.<br>
+          Atme durch die Nase ein und durch den Mund langsam aus, so als würdest du viele Kerzen ausblasen müssen.<br>
+          Wiederhole dies nun fünf Mal, ehe du eine kurze Pause machst. Mache einen zweiten Durchgang.<br>
         </p>
         <p class="lets">-</p>
-        <p class="lets">Übung 1</p>
+        <p class="lets">Zwerchfellübung</p>
         <p class="hometext">
-          Wir bieten eine Auswahl an verschiedenen Spielen, welche dein Gedächtnis und deine Merkfähigkeit wieder in Schwung bringen.<br>
-          In unserem Podcast erzählen Betroffene, wie es ihnen mit der Situation geht und was ihnen besonders geholfen hat. <br>
-          Außerdem könnt ihr euch selbst in unserem Forum mit Gleichgesinnten austauschen und Erfahrungen sammeln bzw. weitergeben. <br>
+          Setz dich aufrecht hin und lege eine Handfläche auf deine Oberschenkel.<br>
+          Schließe deine Augen und atme durch die Nase tief ein.<br>
+          Beim Ausatmen sprich schnell die Buchstaben "p", "t" und "k" mit ihrem Wortlaut aus (also nicht "pe", "te" und "ka").<br>
+          Wiederhole dies nun fünf Mal, ehe du eine kurze Pause machst. Mache einen zweiten Durchgang.<br>
+        </p>
+        <p class="lets">-</p>
+        <p class="lets">Zwerchfellübung 2</p>
+        <p class="hometext">
+          Setz dich aufrecht hin und lege eine Handfläche auf deine Oberschenkel.<br>
+          Schließe deine Augen und atme durch die Nase tief ein.<br>
+          Atme kurz und kräftig dreimal schnell hintereinader aus, ehe du wieder einatmest.<br>
+          Wiederhole dies nun fünf Mal, ehe du eine kurze Pause machst. Mache einen zweiten Durchgang.<br>
         </p>
       </section>
 
